c142f176f72dc2409de03a320050948262f5b14e
peky <ryan_pek@mymail.sutd.edu.sg>
2024-05-08 19:31:12 +0800

    updated oopexample2

--- Week_4/oop_example2_gettersetter_property.py
+++ Week_4/oop_example2_gettersetter_property.py
@@ -14,7 +14,7 @@ class RobotTurtle2:
     @name.setter
     def name(self,value):
         if isinstance(value,str) and value != "":
-            self._name = name
+            self._name = value
 
     @property
     def speed(self):
@@ -23,7 +23,11 @@ class RobotTurtle2:
     @speed.setter
     def speed(self, value):
         if isinstance(value,int) and value > 0:
-            self._speed = speed
+            self._speed = value
+
+    @property
+    def pos(self):
+        return self._pos
 
     def move(self, direction):
         update = {'up' : (self.pos[0], self.pos[1] + self.speed),
@@ -39,7 +43,16 @@ class RobotTurtle2:
 
 
 def main():
-    pass
+    turtle1 = RobotTurtle2("t1")
+    print(turtle1.name)
+    print(turtle1.speed)
+    turtle1.name = 12
+    turtle1.speed = "Aaaaaaaa"
+    print(turtle1.name)
+    print(turtle1.speed)
+    print(turtle1.pos)
+    turtle1.move("up")
+    print(turtle1.pos)
 
 if __name__ == "__main__":
     main()
